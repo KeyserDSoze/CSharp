513909f9453b0ca3985cf32f84cf4e494523632c
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2019-07-16 07:59:48 +0200

    added C# 8

--- CSharp.Library.History/Version8.cs
+++ CSharp.Library.History/Version8.cs
@@ -8,7 +8,42 @@ namespace CSharp.Library.History
     {
         public void Test()
         {
-            
+            IAlreadyImplemented already = new AlreadyImplemented();
+            if (already.IsAdded())
+                Console.WriteLine("Already implemented!!!");
+        }
+    }
+    public class AlreadyImplemented : IAlreadyImplemented
+    {
+        public void Make()
+        {
+            //this.IsAdded(); //I cannot call this method inside another method
+            if (IAlreadyImplemented.Mordor || IAlreadyImplemented.PublicCheck())
+                return;
+        }
+        public override bool base(IAlreadyImplemented).IsAdded()
+        {
+            return false;
+        }
+    }
+    public interface IAlreadyImplemented
+    {
+        void Make();
+        //It's not possible to auto-implement property in interface
+        //public bool FirstCheck { get; set; } = false;
+        public bool IsAdded()
+        {
+            return PublicCheck() || Check();
+        }
+        public static bool Mordor { get; set; }
+        private static bool Check()
+        {
+            return Mordor;
+        }
+        private static bool Asgard = false;
+        public static bool PublicCheck()
+        {
+            return Asgard;
         }
     }
 }
