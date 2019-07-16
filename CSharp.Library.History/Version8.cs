29037d3c9e721239fe042d58f7b9982a38e59a04
Alessandro Rapiti <a.rapiti@vetrya.com>
2019-07-16 17:31:35 +0200

    AR

--- CSharp.Library.History/Version8.cs
+++ CSharp.Library.History/Version8.cs
@@ -12,6 +12,18 @@ namespace CSharp.Library.History
             if (already.IsAdded())
                 Console.WriteLine("Already implemented!!!");
         }
+        public static RGBColor FromRainbow(Rainbow colorBand) =>
+    colorBand switch
+        {
+            Rainbow.Red => new RGBColor(0xFF, 0x00, 0x00),
+            Rainbow.Orange => new RGBColor(0xFF, 0x7F, 0x00),
+            Rainbow.Yellow => new RGBColor(0xFF, 0xFF, 0x00),
+            Rainbow.Green => new RGBColor(0x00, 0xFF, 0x00),
+            Rainbow.Blue => new RGBColor(0x00, 0x00, 0xFF),
+            Rainbow.Indigo => new RGBColor(0x4B, 0x00, 0x82),
+            Rainbow.Violet => new RGBColor(0x94, 0x00, 0xD3),
+            _ => throw new ArgumentException(message: "invalid enum value", paramName: nameof(colorBand)),
+        };
     }
     public class AlreadyImplemented : IAlreadyImplemented
     {
@@ -45,5 +57,23 @@ namespace CSharp.Library.History
         {
             return Asgard;
         }
+    }
+    public struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Distance => Math.Sqrt(X * X + Y * Y);
+        public readonly override string ToString() =>
+            $"({X}, {Y}) is {Distance} from the origin";
+    }
+    public enum Rainbow
+    {
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Indigo,
+        Violet
     }
 }
