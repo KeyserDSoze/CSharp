7cd30470a624926246205421c3b214ed022eeab3
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2019-07-17 13:11:47 +0000

    V8

--- CSharp.Library.History/Version8.cs
+++ CSharp.Library.History/Version8.cs
@@ -13,18 +13,18 @@ namespace CSharp.Library.History
             if (already.IsAdded())
                 Console.WriteLine("Already implemented!!!");
         }
-        public static Color FromRainbow(Rainbow colorBand) =>
-    colorBand switch
-        {
-            Rainbow.Red => Color.FromArgb(0xFF, 0x00, 0x00),
-            Rainbow.Orange => Color.FromArgb(0xFF, 0x7F, 0x00),
-            Rainbow.Yellow => Color.FromArgb(0xFF, 0xFF, 0x00),
-            Rainbow.Green => Color.FromArgb(0x00, 0xFF, 0x00),
-            Rainbow.Blue => Color.FromArgb(0x00, 0x00, 0xFF),
-            Rainbow.Indigo => Color.FromArgb(0x4B, 0x00, 0x82),
-            Rainbow.Violet => Color.FromArgb(0x94, 0x00, 0xD3),
-            _ => throw new ArgumentException(message: "invalid enum value", paramName: nameof(colorBand)),
-        };
+        public static Color FromRainbow(Rainbow colorBand) 
+        => colorBand switch
+            {
+                Rainbow.Red => Color.FromArgb(0xFF, 0x00, 0x00),
+                Rainbow.Orange => Color.FromArgb(0xFF, 0x7F, 0x00),
+                Rainbow.Yellow => Color.FromArgb(0xFF, 0xFF, 0x00),
+                Rainbow.Green => Color.FromArgb(0x00, 0xFF, 0x00),
+                Rainbow.Blue => Color.FromArgb(0x00, 0x00, 0xFF),
+                Rainbow.Indigo => Color.FromArgb(0x4B, 0x00, 0x82),
+                Rainbow.Violet => Color.FromArgb(0x94, 0x00, 0xD3),
+                _ => throw new ArgumentException(message: "invalid enum value", paramName: nameof(colorBand)),
+            };
     }
     public class AlreadyImplemented : IAlreadyImplemented
     {
